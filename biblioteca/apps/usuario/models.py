35f7548084cbd4d50f9a586230d32a587f4909e4
Tuanno <tuannodanyllo26@gmail.com>
2025-02-27 11:36:06 -0300

    Criando o modelo e adicionando o campo CPF

--- biblioteca/apps/usuario/models.py
+++ biblioteca/apps/usuario/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser,Group,Permission
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    cpf = models.CharField(max_length=14, unique=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="usuario_groups",  # Nome alternativo para evitar conflito
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="usuario_permissions",  # Nome alternativo para evitar conflito
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
+    
+
+    
