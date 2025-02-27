270ba96ffc79896cb0c996b8cf1ce625cf082cf8
Tuanno <tuannodanyllo26@gmail.com>
2025-02-27 11:40:49 -0300

    Criando a View do Formulário de Registro

--- biblioteca/apps/usuario/views.py
+++ biblioteca/apps/usuario/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegistroForm
 
-# Create your views here.
+def registrar_usuario(request):
+    if request.method == "POST":
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("login")  # Redireciona após registro
+    else:
+        form = RegistroForm()
+    
+    return render(request, "usuario/register.html", {"form": form})
