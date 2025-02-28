79b68691f3f24d040795d3196ad95469fb666595
Cecilia <ceciliacarolinace2@gmail.com>
2025-02-28 20:37:10 -0300

    Definição das urls e caminhos

--- biblioteca/apps/usuario/urls.py
+++ biblioteca/apps/usuario/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import registrar_usuario
+from .views import registrar_usuario, login_usuario, welcome
 
 app_name = "usuario" 
 
 urlpatterns = [
     path('register/', registrar_usuario, name='register'),
+    path('login/', login_usuario, name='login'),
+    path('welcome/', welcome, name='welcome'),
 ]
