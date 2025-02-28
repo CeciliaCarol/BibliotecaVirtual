a2a156b042bf5b4b964d3f83bbaa881c49f2b6e0
Cecilia <ceciliacarolinace2@gmail.com>
2025-02-28 20:33:54 -0300

    Criação da class LoginForm com cpf e senha

--- biblioteca/apps/usuario/forms.py
+++ biblioteca/apps/usuario/forms.py
@@ -8,3 +8,10 @@ class RegistroForm(UserCreationForm):
     class Meta:
         model = Usuario
         fields = ['username', 'email', 'cpf', 'password1', 'password2']
+
+
+
+class LoginForm(forms.Form):
+    cpf = forms.CharField(max_length=14, required=True, label="CPF")
+    password = forms.CharField(widget=forms.PasswordInput, required=True, label="Senha")
+      
