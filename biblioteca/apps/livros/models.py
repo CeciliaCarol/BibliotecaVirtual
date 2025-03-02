0426b91e47fc181a3e8741061f996d414a3b3485
Tuanno <tuannodanyllo26@gmail.com>
2025-03-02 14:37:42 -0300

    Criado o modelo de Livros

--- biblioteca/apps/livros/models.py
+++ biblioteca/apps/livros/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings  # Importa as configurações do projeto
 
-# Create your models here.
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    generos = models.CharField(max_length=200)
+    paginas = models.IntegerField()
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return self.titulo
